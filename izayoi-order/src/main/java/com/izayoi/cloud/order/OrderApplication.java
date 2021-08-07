be0abd6dcb8f71a4ba0155790bc8196092cd9825
Jim <jimleestone@gmail.com>
2021-08-07 20:49:50 +0800

    add core module

--- izayoi-order/src/main/java/com/izayoi/cloud/order/OrderApplication.java
+++ izayoi-order/src/main/java/com/izayoi/cloud/order/OrderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IzayoiOrderApplication {
+public class OrderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IzayoiOrderApplication.class, args);
+		SpringApplication.run(OrderApplication.class, args);
 	}
 
 }
